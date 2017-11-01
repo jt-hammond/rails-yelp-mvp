eab92fe2522a40e2b78f7ee1b3c4260594e7c300
Jesse Hammond <jessethammond@gmail.com>
2017-11-01 11:03:31 -0400

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = [{
+    name: "Jesse's Diner",
+    address: "Le Petit Italy",
+    category: "italian",
+    phone_number: "416-565-8762"
+  }
+  {
+    name: "Le Chat Blue",
+    address: "43 Avenidad de Americas",
+    category: "french",
+    phone_number: "421-535-4325"
+  }
+  {
+    name: "Keurig",
+    address: "Somewhere",
+    category: "chinese"
+  }
+  {
+    name: "El Pollo Rojo",
+    address: "30 Avenidad de Americas",
+    category: "french",
+    phone_number: "421-535-4525"
+  }
+  {
+    name: "Ikea",
+    address: "10 Mesa Calle",
+    category: "belgian"
+  }
+]
+
+Restaurant.create(restaurants)
